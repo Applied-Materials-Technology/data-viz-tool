5b83232eae60d0ebbca354b5b1e14041e3f6e033
meganasampson <megan.anne.sampson@gmail.com>
2024-07-31 13:38:38 +0100

    clean up and added reqs

--- main.py
+++ main.py
@@ -8,25 +8,17 @@ import numpy as np
 import pyvista as pv
 import os
 import random
- 
 import time
-start_time = time.time()
 
-#print("--- %s seconds ---" % (time.time() - start_time))
-#g = pv.read(Path(os.path.join(Path.cwd(), 'data/OptSpeckle_5Mpx_2464_2056_width5_8bit_GBlur1.tiff')))
+########### requires pycdata ############
 
-counter = 0
-p = pv.Plotter()
-#p.add_mesh(g, opacity=0.5, name='data', cmap='gist_ncar')
-#p.show(interactive_update=True)
-"""
-def show(new_path):
-    g = pv.read(Path(os.path.join(new_path)))
-    p.add_mesh(g, opacity=0.5, name='data', cmap='gist_ncar')"""
+start_time = time.time() # get base time to start timer
+
+p = pv.Plotter() # create plotter for pyvista
 
 
-class OnMyWatch:
-    watchDirectory = Path("example_generate")
+class Watcher:
+    watchDirectory = Path("example_generate") # path where data is being read from
  
     def __init__(self):
         self.observer = Observer()
@@ -46,6 +38,10 @@ class OnMyWatch:
  
  
 class Handler(FileSystemEventHandler):
+
+    """
+    Decide what to do when certain events are detected in the watchDirectory
+    """
  
     @staticmethod
     def on_any_event(event):
@@ -54,24 +50,22 @@ class Handler(FileSystemEventHandler):
  
         elif event.event_type == 'created':
             print("Watchdog received created event - % s." % event.src_path)
+
         elif event.event_type == 'modified':
-            print("Watchdog received modified event - % s." % event.src_path)
+            print("Watchdog received modified event - % s." % event.src_path)  
             try:
-                #p.update()
                 g = pv.read(Path(os.path.join(event.src_path)))
                 print(event.src_path)
                 print(time.time() - start_time)
-                #print("file is okay")
-                p.add_mesh(g, opacity=0.5, name='data', cmap='gist_ncar')
-                p.show(interactive_update=True)
-                counter = counter + 1
-                print(counter)
+                print("FILE ACCEPTED")
+                p.add_mesh(g, opacity=0.5, name='data', cmap='gist_ncar') # add the data from new file to the plotter
+                p.show(interactive=True, interactive_update = True)
+                p.update()
             except:
-                print("WAITING FOR FILE TRANSFER....")
-                #p.update()
+                print("WAITING FOR FILE TRANSFER....") # error occurs when trying to open a file before it's fully uploaded
              
 
 if __name__ == '__main__':
-    watch = OnMyWatch()
+    watch = Watcher()
     watch.run()
 
