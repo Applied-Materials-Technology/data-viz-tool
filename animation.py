49d34dd986f845eb478692fd1a51a8fa0de4dd2e
meganasampson <megan.anne.sampson@gmail.com>
2024-10-22 11:14:22 +0100

    outline of example animation

---
+++ animation.py
@@ -0,0 +1,58 @@
+import pyvista as pv
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+
+from matplotlib.colors import ListedColormap
+
+
+csv_path = "data/csvs"
+files = ["data/csvs/Image_0000_0.tiff.csv", "data/csvs/Image_0001_0.tiff.csv", "data/csvs/Image_0002_0.tiff.csv"]
+
+
+points_csv = []
+p = pv.Plotter(shape=(1,1))
+
+
+p.subplot(0, 0)
+p.show(interactive_update=True)
+
+
+def read_csv(file):
+    raw_data = pd.read_csv(file, header=0)
+
+    data2 = raw_data[['X[mm]', 'Y[mm]', 'Z[mm]', 'Strain-global frame: Exx', 'Strain-global frame: Eyy']]
+    #data2.to_csv('newcsvfile.csv', index=False)
+
+
+
+    for i in range(len(raw_data['X[mm]'])):
+        print(raw_data['X[mm]'][i])
+        pointstemp = [raw_data['X[mm]'][i], raw_data['Y[mm]'][i], raw_data['Z[mm]'][i]]
+        points_csv.append(pointstemp)
+
+    meshcsv = pv.PolyData(points_csv, force_float = False)
+    show_csv(meshcsv, raw_data)
+
+def show_csv(meshcsv, raw_data):
+    p.clear()
+    p.update()
+    labels = dict(zlabel='Z', xlabel='X', ylabel='Y')
+    #p.add_mesh(meshcsv)
+    #p.add_mesh(meshcsv, scalars = raw_data['Strain-global frame: Eyy'],show_scalar_bar=False)
+    p.add_mesh(meshcsv)
+    p.show_grid(**labels)
+    p.add_axes(**labels)
+
+    p.add_scalar_bar(
+
+    'Insert label')
+
+
+    p.show(interactive_update=True)
+    p.update()
+    time.sleep(5)
+
+for file in files:
+    read_csv(file)
