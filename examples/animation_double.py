d0df49222419d0b59f6172f38798a36570409cf9
meganasampson <megan.anne.sampson@gmail.com>
2024-12-09 15:29:21 +0000

    renderer experiments

--- examples/animation_double.py
+++ examples/animation_double.py
@@ -22,14 +22,24 @@ files_set2 = [Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0006_0.tiff.c
 
 
 p = pv.Plotter(shape=(1,2))
-print(p.render_window)
+
+rwi = pv.RenderWindowInteractor(p)
+
+
+#print(p.render_window.IsCurrent())
 p2 = pv.Plotter(shape=(1,1))
-print(p2.render_window.IsCurrent())
-
-
+renderer1 = p.renderers
+print(renderer1)
+print(renderer1._plotter)
+print(renderer1.active_renderer)
+#print(dir(renderer1))
+#rwi.set_render_window(p.render_window)
+#print(dir(rwi))
 #p.subplot(0, 0)
-p.show(interactive_update=True)
-print(p.render_window.IsCurrent())
+#p2.show(interactive_update=True)
+#p.show(interactive_update=True)
+#print(p2.render_window.IsCurrent())
+#print(dir(p))
 
 
 
---
+++ examples/rendertest.py
@@ -0,0 +1,30 @@
+import pyvista as pv
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+from pathlib import Path
+import os
+
+from matplotlib.colors import ListedColormap
+
+
+p = pv.Plotter(shape=(1,2))
+
+rwi = pv.RenderWindowInteractor(p)
+
+
+#print(p.render_window.IsCurrent())
+p2 = pv.Plotter(shape=(1,1))
+renderer1 = p.renderers
+print(renderer1)
+print(renderer1._plotter)
+print(renderer1.active_renderer)
+#print(dir(renderer1))
+#rwi.set_render_window(p.render_window)
+#print(dir(rwi))
+#p.subplot(0, 0)
+#p2.show(interactive_update=True)
+#p.show(interactive_update=True)
+#print(p2.render_window.IsCurrent())
+#print(dir(p))
