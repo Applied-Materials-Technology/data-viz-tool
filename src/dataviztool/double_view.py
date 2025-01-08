6232774ef3d8eea3cdd3ec2691402addf5cecdef
meganasampson <megan.anne.sampson@gmail.com>
2025-01-08 11:25:31 +0000

    allow user to specify what to read from a csv

--- src/dataviztool/double_view.py
+++ src/dataviztool/double_view.py
@@ -15,10 +15,7 @@ import asyncio
 ########### full example requires pycdata ############
 
 start_time = time.time() # get base time to start timer
-#p = pv.Plotter(shape=(1,2)) # create plotter for pyvista
 
-def test_function(texttoprint):
-    print(texttoprint)
 
 class WatcherCSV:
 
@@ -53,15 +50,8 @@ class HandlerCSV(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
         #print(event.src_path)
-        path1 = os.path.dirname(event.src_path)
-        path2 = os.path.basename(path1)
 
-        """Change subplot for different data"""
-        if path2 == "right":
-            subploty = 1
-        elif path2 == "left":
-            subploty = 0
-
+        displayer.subplot_decide(event.src_path)
 
         if event.is_directory:
             return None
@@ -76,34 +66,45 @@ class HandlerCSV(FileSystemEventHandler):
 
             """For data in csv format, e.g. example csvs """  
             if 'csv' in event.src_path:
-                displayer.csv_displayer(subploty, event.src_path)
+                #displayer.csv_displayer(subploty, event.src_path)
+                displayer.csv_displayer(event.src_path)
             else:
                 """For reading tiff files"""
-                displayer.tiff_displayer(subploty, event.src_path)
+                #displayer.tiff_displayer(subploty, event.src_path)
+                displayer.tiff_displayer(event.src_path)
 
 class Displayer():
-    def __init__(self, p) -> None:
+    def __init__(self, p, subploty: int = 1, x_coord = "", y_coord = "", z_coord = "", colours = "") -> None:
         self.p = p
+        self.subploty = subploty
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.z_coord = z_coord
+        self.colours = colours
 
-    def csv_displayer(self, subploty, event):
+    def subplot_decide(self, event):
+        path1 = os.path.dirname(event)
+        path2 = os.path.basename(path1)
+
+        """Change subplot for different data"""
+        if path2 == "right":
+            self.subploty = 1
+        elif path2 == "left":
+            self.subploty = 0
+
+    def csv_displayer(self, event):
         try:
-            print("YOU ARE HERE")
-            self.p.subplot(0, subploty)
+            self.p.subplot(0, self.subploty)
             points_csv = []
             raw_data = pd.read_csv(Path(os.path.join(event)), header=0)
-            #data2 = raw_data[['X[mm]', 'Y[mm]', 'Z[mm]', 'Strain-global frame: Exx', 'Strain-global frame: Eyy']]
-            data2 = raw_data[['coor.X [mm]', 'coor.Y [mm]' ,'coor.Z [mm]', 'disp.Horizontal Displacement U [mm]']]
+            data2 = raw_data[[self.x_coord, self.y_coord, self.z_coord, self.colours]]
 
-            #for i in range(len(raw_data['X[mm]'])):
-            for i in range(len(raw_data['coor.X [mm]'])):
-                #pointstemp = [raw_data['X[mm]'][i], raw_data['Y[mm]'][i], raw_data['Z[mm]'][i]]
-                pointstemp = [raw_data['coor.X [mm]'][i], raw_data['coor.Y [mm]'][i], raw_data['coor.Z [mm]'][i]]
+
+            for i in range(len(raw_data[self.x_coord])):
+                pointstemp = [raw_data[self.x_coord][i], raw_data[self.y_coord][i], raw_data[self.z_coord][i]]
                 points_csv.append(pointstemp)
-            print("YOU ARE HERE 2")
             meshcsv = pv.PolyData(points_csv, force_float = False)
 
-            #p.camera_position = "xy"
-            #p.add_mesh(meshcsv, scalars = raw_data['Strain-global frame: Eyy'],show_scalar_bar=False)
             self.p.add_mesh(meshcsv, scalars = raw_data['disp.Horizontal Displacement U [mm]'],show_scalar_bar=False, interpolate_before_map = False)
 
             labels = dict(ztitle='Z', xtitle='X', ytitle='Y')
@@ -114,13 +115,12 @@ class Displayer():
             self.p.camera_position = "xy"
 
             self.p.show(interactive=True, interactive_update = True)
-            #p.update()
         except:
             pass
 
-    def tiff_displayer(self, subploty, event):
+    def tiff_displayer(self, event):
         try:
-            self.p.subplot(0,subploty)
+            self.p.subplot(0,self.subploty)
             g = pv.read(Path(os.path.join(event)))
             self.p.camera_position = "xy"
             self.p.add_mesh(g, opacity=0.5, name='data', cmap='gist_ncar') # add the data from new file to the plotter
@@ -129,10 +129,18 @@ class Displayer():
         except:
             pass
 
+    def load_csv(self, choose_x, choose_y, choose_z, choose_c):
+        print("getting csv details...")
+        self.x_coord = choose_x
+        self.y_coord = choose_y
+        self.z_coord = choose_z
+        self.colours = choose_c
+
 
 
 if __name__ == '__main__':
     watch = WatcherCSV()
     displayer = Displayer(pv.Plotter(shape=(1,2)))
+    displayer.load_csv('coor.X [mm]', 'coor.Y [mm]' ,'coor.Z [mm]', 'disp.Horizontal Displacement U [mm]')
     watch.run()
 
