19bb9ce7e6d304c26f509df52c90f779226b9cc3
meganasampson <megan.anne.sampson@gmail.com>
2025-01-14 10:04:22 +0000

    changed files for double animation example

--- examples/animation_double.py
+++ examples/animation_double.py
@@ -13,18 +13,23 @@ from matplotlib.colors import ListedColormap
 
 
 csv_path = "data/csvs"
-files_set1 = [Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0001_0.tiff.csv")), Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0002_0.tiff.csv")),
+csv_path2 = "data/Data_viz"
+half_path = os.path.join(Path.cwd().parent, "data/Data_viz")
+files_set1_old = [Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0001_0.tiff.csv")), Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0002_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0003_0.tiff.csv")),Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0004_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0005_0.tiff.csv"))]
-files_set2 = [Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0006_0.tiff.csv")),
+files_set2_old = [Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0006_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0007_0.tiff.csv")),Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0008_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0009_0.tiff.csv")),Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0010_0.tiff.csv"))]
-file_set_full = [Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0001_0.tiff.csv")), Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0002_0.tiff.csv")),
+file_set_full_old = [Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0001_0.tiff.csv")), Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0002_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0003_0.tiff.csv")),Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0004_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0005_0.tiff.csv")), Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0006_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0007_0.tiff.csv")),Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0008_0.tiff.csv")),
          Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0009_0.tiff.csv")),Path(os.path.join(Path.cwd().parent,"data/csvs/Image_0010_0.tiff.csv"))]
 
+file_names = sorted([ f for f in os.listdir(Path(os.path.join(Path.cwd().parent, csv_path2)))])
+file_set_full = [Path(os.path.join(half_path, i)) for i in file_names]
+#print(file_set_full2)
 
 p = pv.Plotter(shape=(1,2))
 
@@ -65,5 +70,44 @@ def display():
         read_csv(file, 0)
         read_csv(file,1)
 
-display()
 
+#display()
+
+def csv_displayer(file, subploty):
+
+    x_coord = 'coor.X [mm]'
+    y_coord = 'coor.Y [mm]'
+    z_coord = 'coor.Z [mm]'
+    colours = 'disp.Horizontal Displacement U [mm]'
+
+    p.subplot(0, subploty)
+    points_csv = []
+    #raw_data = pd.read_csv(Path(os.path.join(event)), header=0)
+    raw_data = pd.read_csv(file)
+    data2 = raw_data[[x_coord, y_coord, z_coord, colours]]
+
+
+    for i in range(len(raw_data[x_coord])):
+        pointstemp = [raw_data[x_coord][i], raw_data[y_coord][i], raw_data[z_coord][i]]
+        points_csv.append(pointstemp)
+    meshcsv = pv.PolyData(points_csv, force_float = False)
+
+
+    p.add_mesh(meshcsv, scalars = raw_data[colours],show_scalar_bar=False, interpolate_before_map = False)
+
+    labels = dict(ztitle='Z', xtitle='X', ytitle='Y')
+    p.show_bounds(**labels)
+
+    p.add_scalar_bar('Label')
+
+    p.camera_position = "xy"
+
+    p.show(interactive=True, interactive_update = True)
+
+
+def display2():
+    for file in file_set_full:
+        csv_displayer(file, 0)
+        csv_displayer(file,1)
+
+display2()
