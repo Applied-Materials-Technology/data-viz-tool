1783387df488b08486eafbe0402b0c464b20674f
meganasampson <megan.anne.sampson@gmail.com>
2025-06-10 14:04:21 +0100

    logger update

--- src/dataviztool/logger.py
+++ src/dataviztool/logger.py
@@ -1,25 +1,30 @@
 import logging
 import sys
 
+
 logger = logging.getLogger(__name__)
+
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
 file_handler = logging.FileHandler('logs.log')
+
+console_handler = logging.StreamHandler()
+
 file_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.DEBUG)
 
-stdout_handler = logging.StreamHandler()
-stdout_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-
-stdout_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warn message')
-logger.error('error message')
-logger.critical('critical message')
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 
+logger.debug('This is a debug message')
+logger.info('This is an info message')
+logger.warning('This is a warning message')
+logger.error('This is an error message')
+logger.critical('This is a critical message')
+logger.exception('An exception')
