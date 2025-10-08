a3603c6aa5c476c8053269919e86cbdb119e309b
meganasampson <megan.anne.sampson@gmail.com>
2025-10-08 15:40:48 +0100

    added some tests for plotter creation

--- examples/ex3_manual_plotter.py
+++ examples/ex3_manual_plotter.py
@@ -20,9 +20,6 @@ my_displayer.create_plotter(1,2)
 my_displayer.assign_subplot(0,0,"Experimental View Manual", "left")
 my_displayer.assign_subplot(0,1,"Simulation View Manual", "right")
 
-print(my_displayer.display_opts.x_coord)
-print(my_displayer.watch_path)
-
 my_watcher = Watcher(displayer = my_displayer, watch_path="example_inputloc")
 
 
--- tests/test_display_tools.py
+++ tests/test_display_tools.py
@@ -1,11 +1,31 @@
 import pytest
-#from dataviztool import dataviztool
 import dataviztool
 
-explotter = dataviztool.Displayer(automake_plotter=False)
-
-
 def test_auto_create_plotter():
-    #dataviztool.subplot_decider(explotter, "event")
-    assert 1 == 1
 
+    explotter = dataviztool.Displayer(automake_plotter=True, watch_path="test_inputloc")
+    assert explotter.p is not None
+
+def test_no_auto_create():
+
+    explotter = dataviztool.Displayer(automake_plotter=False)
+    assert explotter.p == None
+
+def test_manual_create_plotter_size():
+
+    explotter = dataviztool.Displayer(automake_plotter=False)
+    explotter.create_plotter(1,2)
+    assert explotter.p.shape == (1,2)
+
+
+subplot_assignment = [("left", [0,0]),
+            ("right", [0,1]),]
+
+@pytest.mark.parametrize("x ,expected", subplot_assignment)
+def test_manual_subplot_assign(x, expected):
+
+    explotter = dataviztool.Displayer(automake_plotter=False, watch_path="test_inputloc")
+    explotter.create_plotter(1,2)
+    explotter.assign_subplot(0,0,"Experimental", "left")
+    explotter.assign_subplot(0,1,"Simulation", "right")
+    assert explotter._subplot_dict[x] == expected
